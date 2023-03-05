da4530bf9026143c526a4e03593a15cfc4224ab8
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-03-04 21:50:02 -0300

    Entendendo do Comando final

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,17 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+
+        final String UF = "SP";
+        final String BR = "Brasil";
+        final double PI = 3.14;
+        final int ESTADOS_BRASILEIROS = 27;
+        final int ANO_2000 = 2000;
+
+        System.out.println(UF);
+        System.out.println(BR);
+        System.out.println(PI);
+        System.out.println(ESTADOS_BRASILEIROS);
+        System.out.println(ANO_2000);
+    }
+}
